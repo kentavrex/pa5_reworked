30f83246378b5bf7c18311cad9622aff0376491f
Ханов Амир Асадович <khanov@tochka.com>
2025-01-05 15:27:52 +0300

    first

--- cs.c
+++ cs.c
@@ -4,59 +4,53 @@
 #include <unistd.h>
 #include "cs.h"
 
-static Message create_message(timestamp_t time, MessageType type) {
-    Message message = {
-        .s_header = {
-            .s_local_time = time,
-            .s_type = type,
-            .s_magic = MESSAGE_MAGIC,
-            .s_payload_len = 0
-        }
-    };
-    return message;
-}
-
-static void send_message_to_peers(const Process* proc, const Message* message, bool skip_self) {
-    for (int peer = 1; peer < proc->num_process; peer++) {
-        if (skip_self && peer == proc->pid) {
-            continue;
-        }
-        if (send(proc, peer, message) == -1) {
-            fprintf(stderr, "Error: Process %d failed to send message to process %d\n", proc->pid, peer);
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
-static void update_request_timestamp(Process* proc, timestamp_t time) {
-    proc->rep[proc->pid - 1] = time;
-}
-
-static void reset_peer_timestamps(Process* proc) {
-    proc->rep[proc->pid - 1] = 0;
-    for (int peer = 1; peer < proc->num_process; peer++) {
-        proc->rep[peer - 1] = 0;
-    }
-}
 
 int send_critical_section_request(const void* context) {
     Process* proc = (Process*) context;
     timestamp_t current_time = increment_lamport_time();
-    Message request_message = create_message(current_time, CS_REQUEST);
+    Message request_message = {
+        .s_header = {
+            .s_local_time = current_time,
+            .s_type = CS_REQUEST,
+            .s_magic = MESSAGE_MAGIC,
+            .s_payload_len = 0
+        }
+    };
 
-    update_request_timestamp(proc, current_time);
-    send_message_to_peers(proc, &request_message, true);
-
+    proc->rep[proc->pid - 1] = current_time;
+    for (int peer = 1; peer < proc->num_process; peer++) {
+        if (peer != proc->pid) {
+            if (send(proc, peer, &request_message) == -1) {
+                fprintf(stderr, "Error: Process %d failed to send CS_REQUEST to process %d\n", proc->pid, peer);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
     return 0;
 }
 
 int send_critical_section_release(const void* context) {
     Process* proc = (Process*) context;
     timestamp_t current_time = increment_lamport_time();
-    Message release_message = create_message(current_time, CS_REPLY);
 
-    reset_peer_timestamps(proc);
-    send_message_to_peers(proc, &release_message, true);
+    Message release_message = {
+        .s_header = {
+            .s_local_time = current_time,
+            .s_type = CS_REPLY,
+            .s_magic = MESSAGE_MAGIC,
+            .s_payload_len = 0
+        }
+    };
 
+    proc->rep[proc->pid - 1] = 0;
+    for (int peer = 1; peer < proc->num_process; peer++) {
+        if (peer != proc->pid && proc->rep[peer - 1]) {
+            if (send(proc, peer, &release_message) == -1) {
+                fprintf(stderr, "Error: Process %d failed to send CS_RELEASE to process %d\n", proc->pid, peer);
+                exit(EXIT_FAILURE);
+            }
+            proc->rep[peer - 1] = 0;
+        }
+    }
     return 0;
 }
