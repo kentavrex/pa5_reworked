dc9799f75eb6ad2165bd36b8fece5567136b276b
Ханов Амир Асадович <khanov@tochka.com>
2025-01-07 18:20:01 +0300

    new

--- channel.h
+++ channel.h
@@ -13,21 +13,26 @@
 #include <fcntl.h>
 
 
-struct channel {
-    int end_id;
-    int descriptor;
-    struct channel* next_channel;
-};
+struct channel* create_channel(int end_id, int descriptor);
+void close_channel(struct channel* channel);
 
 struct mutex_request {
     int id;
     timestamp_t time;
 };
 
+
+struct channel {
+    int end_id;
+    int descriptor;
+    struct channel* next_channel;
+};
+
 struct mutex_queue {
     size_t length;
     struct mutex_request requests[20];
 };
+
 
 struct process {
     int id;
@@ -37,13 +42,10 @@ struct process {
     struct channel* write_channel;
     struct channel* read_channel;
 };
-
-struct channel* create_channel(int end_id, int descriptor);
-void close_channel(struct channel* channel);
-void add_read_channel(struct process* process, struct channel* read_channel);
-void add_write_channel(struct process* process, struct channel* write_channel);
-int get_channel(struct process* process, int8_t end_id, bool isForRead);
-int create_pipes(struct process* processes, int X);
 void close_other_processes_channels(int process_id, struct process* processes);
+int get_channel(struct process* process, int8_t end_id, bool isForRead);
+void add_write_channel(struct process* process, struct channel* write_channel);
+int create_pipes(struct process* processes, int X);
+void add_read_channel(struct process* process, struct channel* read_channel);
 
 #endif
--- ipc.c
+++ ipc.c
@@ -78,23 +78,38 @@ int receive(void* self, local_id from, Message* msg) {
 }
 
 
+ssize_t read_header(int descriptor, Message* msg) {
+    size_t header_size = sizeof(msg->s_header);
+    return read(descriptor, &msg->s_header, header_size);
+}
+
+ssize_t read_payload(int descriptor, Message* msg, size_t msg_size) {
+    return read(descriptor, &msg->s_payload, msg_size);
+}
+
+int receive_from_channel(struct channel* read_channel, Message* msg) {
+    ssize_t bytes_num = read_header(read_channel->descriptor, msg);
+    if (bytes_num > 0) {
+        size_t msg_size = msg->s_header.s_payload_len;
+        bytes_num = read_payload(read_channel->descriptor, msg, msg_size);
+        if (bytes_num >= 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int receive_any(void* self, Message* msg) {
-    
     struct process* process = (struct process*) self;
     struct channel* read_channel = process->read_channel;
 
     while (read_channel != NULL) {
-        size_t header_size = sizeof(msg->s_header);
-        ssize_t bytes_num = read(read_channel->descriptor, &msg->s_header, header_size);
-
-        if (bytes_num > 0) {
-            size_t msg_size = msg->s_header.s_payload_len;
-            bytes_num = read(read_channel->descriptor, &msg->s_payload, msg_size);
-            if (bytes_num >= 0) {
-                return 0;
-            }
+        if (receive_from_channel(read_channel, msg) == 0) {
+            return 0;
         }
         read_channel = read_channel->next_channel;
     }
+
     return 1;
 }
+
